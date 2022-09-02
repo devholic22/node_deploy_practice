4e8fee4b1ec497a36da3bda53c251e69c9e0a3f0
Hyunjoon Choi <90085154+devholic22@users.noreply.github.com>
2022-09-02 22:12:31 +0900

    git pull deploy test

--- src/routers/globalRouter.js
+++ src/routers/globalRouter.js
@@ -3,7 +3,7 @@ import { Router } from "express";
 const router = Router();
 
 router.get("/", (req, res) => {
-  return res.send("hello world");
+  return res.send("hello world (edited)");
 });
 
 export default router;
